390b062150b5da979c910fed51ec0a6bfcef5591
Sergey Kravchenko <kravchenko.sgv@gmail.com>
2020-10-01 15:40:41 +0300

    9.2. Checkstyle [#374318]

--- src/ru/job4j/calculator/Fit.java
+++ src/ru/job4j/calculator/Fit.java
@@ -8,7 +8,7 @@ public class Fit {
     }
 
     public static double womanWeight(short height) {
-        double rsl = (height - 110) * 1.15;;
+        double rsl = (height - 110) * 1.15;
         return rsl;
     }
 
--- src/ru/job4j/condition/SqArea.java
+++ src/ru/job4j/condition/SqArea.java
@@ -3,8 +3,7 @@ package ru.job4j.condition;
 public class SqArea {
     public static double square(int p, int k) {
         double h = p / (2 * (k + 1));
-        double L = h * k;
-        return L * h;
+        return h * k * h;
     }
 
     public static void main(String[] args) {
